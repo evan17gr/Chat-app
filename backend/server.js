456186745e49cb1b85c71bcd6c1bf15c5ab23b52
Evan Kalvis <evankalvis@hotmail.com>
2020-12-07 23:34:34 +0000

    Setup a server

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const server = require('http');
+const io = require('socket.io');
+const { createServer } = require('tls');
+
+const app = express();
+
+const server = createServer(app);
+
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+    console.log(`Successful server setup on port ${PORT}`);
+});
